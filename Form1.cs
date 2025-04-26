df587f281ff6e61510459a3ac4dc1f05c428ead0
mohamedaladawy <mohamedaladawy9632147@gmail.com>
2025-04-26 22:38:45 +0300

    First Commit From Abdelsatter

--- Form1.cs
+++ Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Oracle.DataAccess.Client;
 using Oracle.DataAccess.Types;
+//Abdelsatter
 namespace Project_SW
 {
     public partial class Form1 : Form
