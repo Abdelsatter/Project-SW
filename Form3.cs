8a318aee40a7da285e0652013cc04619e7d214a0
youssef <youssefahmed4869@gmail.com>
2025-05-02 02:08:47 +0300

    lsa hgrb el crystal

--- Form3.cs
+++ Form3.cs
@@ -9,13 +9,42 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Oracle.DataAccess.Client;
 using Oracle.DataAccess.Types;
+using CrystalDecisions.Shared;
 namespace Project_SW
 {
     public partial class Form3 : Form
     {
+
+        CrystalReport1 CR;
+
         public Form3()
         {
             InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            CR = new CrystalReport1();
+            foreach (ParameterDiscreteValue v in CR.ParameterFields[0].DefaultValues)
+                comboBox1.Items.Add(v.Value);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CR.SetParameterValue(0, comboBox1.Text);
+            CR.SetParameterValue(1, Convert.ToDateTime(textBox1));
+            CR.SetParameterValue(2, Convert.ToDateTime(textBox2));
+            crystalReportViewer1.ReportSource = CR;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
